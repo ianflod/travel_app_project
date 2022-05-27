e540f95cbde6cf11b3c8488f794d4556b181d1af
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro.local>
2022-05-27 17:56:00 +0100

    further set-up

--- models/city.py
+++ models/city.py
@@ -1,3 +1,12 @@
 class City:
 
-    def __init__()
+    def __init__(self, name, country, completed = False, id = None):
+        self.name = name
+        self.country = country
+        self.completed = completed
+        self.id = id
+
+    def mark_completed(self):
+        self.completed = True
+
+        
